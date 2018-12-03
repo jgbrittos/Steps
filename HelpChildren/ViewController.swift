593246fe8c95b5017a7ce1919fd0d8a9b6ed3d49
João Gabriel <joaogabrielsilva@wizsolucoes.com.br>
2018-12-03 09:18:20 -0200

    Initial Commit

---
+++ HelpChildren/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelpChildren
+//
+//  Created by João Gabriel on 03/12/18.
+//  Copyright © 2018 João Gabriel de Britto e Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
