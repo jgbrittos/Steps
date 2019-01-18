c2bec3317ac07c3d6f4935d712e937750d8a137d
João Gabriel <joaogabrielsilva@wizsolucoes.com.br>
2019-01-18 20:01:51 -0200

    Adding images and sounds

--- HelpChildren/ViewModel/LevelViewModel.swift
+++ HelpChildren/ViewModel/LevelViewModel.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class LevelViewModel {
+    private let objects = ["baby", "dog", "door", "rain", "pig"]
     
     var levels: [Activity] = [Activity]()
     var updateMenu: (()->())?
@@ -27,9 +28,28 @@ class LevelViewModel {
         
         levels = data.levels
         menu = data.menu
+//        levels = data.levels.map { (activity) in
+//            var newActivity = Activity()
+//            newActivity = activity
+//
+//            let random = generateRandomObject(except: activity.image1)
+//            if activity.image2 == "random" {
+//                newActivity.image2 = random
+//            } else if activity.image3 == "random" {
+//                newActivity.image3 = random
+//            } else {
+//                return activity
+//            }
+//
+//            return newActivity
+//        }
     }
     
     func filterBy(level: Int) -> [Activity] {
         return self.levels.filter { $0.level == level }
     }
+    
+//    func generateRandomObject(except: String) -> String {
+//        return objects.filter { $0 != except }.randomElement()!
+//    }
 }
